3991c73a0f9307317997e714451bfcbcdf7c1420
cbonesteel <cambones12@gmail.com>
2023-02-04 14:22:57 -0500

    setup structure

--- application.py
+++ application.py
@@ -11,5 +11,13 @@ app = Flask(__name__,
 def index():
     return render_template('index.html')
 
+@app.route('/map')
+def map():
+    return render_template('map/map.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error/404.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
