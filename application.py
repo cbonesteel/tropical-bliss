ea38474f71271113c5886ad6cb22bd2cee8c0b29
Aiden Hammond <amh37629@uga.edu>
2023-02-05 03:05:14 -0500

    playing with application.py

--- application.py
+++ application.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import csv
+import json
+
+bigDict = {}
 
 app = Flask(__name__, 
     static_url_path='', 
@@ -14,7 +17,7 @@ def index():
 
 @app.route('/map')
 def map():
-    return render_template('map/map.html')
+    return render_template('map/map.html', data=bigDict)
 
 @app.route('/data')
 def data():
@@ -73,83 +76,62 @@ def survey():
             crimeW = weights[3]
         elif (request.form['crime4']):
             crimeW = weights[4]
-    states = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-              'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-              'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-              'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-              'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
-    statesWeather = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-              'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-              'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-              'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-              'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
-    statesCrime = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-              'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-              'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-              'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-              'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
-    statesHealth = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-              'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-              'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-              'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-              'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
-    statesEdu = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-              'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-              'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-              'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-              'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
+        states = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
+                'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
+                'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
+                'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
+                'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
+        
+        def parseCSV(file, addsub, weight):
+            statesReturn = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
+                    'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
+                    'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
+                    'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
+                    'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
+            with open(file) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                linecount = 0
+                for row in csv_reader:
+                    if linecount == 1:
+                        #print(row)
+                        max = float(row[0])
+                    if linecount == 50:
+                        min = float(row[0])
+                    linecount += 1
+                linecount = 0
+                csv_file.seek(0)
+                for row in csv_reader:
+                    if linecount == 0:
+                        pass
+                    else:
+                        statesReturn[row[1]] += addsub * (float(row[0]) * weight - min) / (max - min)
+                        states[row[1]] += addsub * (float(row[0]) * weight - min) / (max - min)
+                    linecount += 1
+                return statesReturn
 
-    def parseCSV(file, addsub, weight):
-        statesReturn = {'AL': 0, 'AK': 0, 'AZ': 0, 'AR': 0, 'CA': 0, 'CO': 0, 'CT': 0, 'DE': 0, 'FL': 0, 'GA': 0, 'HI': 0,
-                  'ID': 0, 'IL': 0, 'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0, 'MI': 0,
-                  'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0, 'NJ': 0, 'NM': 0, 'NY': 0, 'NC': 0,
-                  'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0, 'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0,
-                  'VT': 0, 'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
-        with open(file) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            linecount = 0
-            for row in csv_reader:
-                if linecount == 1:
-                    #print(row)
-                    max = float(row[0])
-                if linecount == 50:
-                    min = float(row[0])
-                linecount += 1
-            linecount = 0
-            csv_file.seek(0)
-            for row in csv_reader:
-                if linecount == 0:
-                    pass
-                else:
-                    statesReturn[row[1]] += addsub * (float(row[0]) * weight - min) / (max - min)
-                    states[row[1]] += addsub * (float(row[0]) * weight - min) / (max - min)
-                linecount += 1
-            return statesReturn
-
-    #states += parseCSV('db/education.csv', 1, teacherW)
-    #states += parseCSV('db/crime.csv', -1, crimeW)
-    #states += parseCSV('db/death.csv', -1, healthW)
-    #states += parseCSV('db/weather.csv', 1, climateW)
-    statesEdu = parseCSV('db/education.csv', 1, teacherW)
-    statesCrime = parseCSV('db/crime.csv', -1, crimeW)
-    statesHealth = parseCSV('db/death.csv', -1, healthW)
-    statesWeather = parseCSV('db/weather.csv', 1, climateW)
+        #states += parseCSV('db/education.csv', 1, teacherW)
+        #states += parseCSV('db/crime.csv', -1, crimeW)
+        #states += parseCSV('db/death.csv', -1, healthW)
+        #states += parseCSV('db/weather.csv', 1, climateW)
+        statesEdu = parseCSV('db/education.csv', 1, teacherW)
+        statesCrime = parseCSV('db/crime.csv', -1, crimeW)
+        statesHealth = parseCSV('db/death.csv', -1, healthW)
+        statesWeather = parseCSV('db/weather.csv', 1, climateW)
 
 
 
-    print(states)
-    print(statesEdu)
-    print(statesCrime)
-    print(statesHealth)
-    print(statesWeather)
-    state_names= [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
+        print(statesEdu)
+        print(statesCrime)
+        print(statesHealth)
+        print(statesWeather)
+        state_names= [ 'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
-    bigDict = {}
+        for name in state_names:
+            #print(key)
+            bigDict[name] = {'crime': statesCrime[name], 'climate': statesWeather[name], 'health': statesHealth[name], 'education': statesEdu[name], 'score': states[name]}
+        print(bigDict)
 
-    for name in state_names:
-        #print(key)
-        bigDict[name] = {'crime': statesCrime[name], 'climate': statesWeather[name], 'health': statesHealth[name], 'education': statesEdu[name], 'score': states[name]}
-    print(bigDict)
+        return redirect('/map')
 
     return render_template('survey.html')
 
